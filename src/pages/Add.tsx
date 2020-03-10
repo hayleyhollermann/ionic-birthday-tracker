6e87b6a2287a076baffc31ce48b0025610c35728
Hayley Hollermann <hayleyhollermann@gmail.com>
2020-03-10 15:57:28 -0500

    set up basic layout for AddBirthday

--- src/pages/Add.tsx
+++ src/pages/Add.tsx
@@ -1,26 +1,43 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItemDivider, IonItem, IonInput, IonButton } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
-const Add: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Add A Birthday</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Add A Birthday</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+import { BirthdayStore } from "./BirthdayStore";
+
+type HomeProps = {
+    birthdayStore: BirthdayStore,
+};
+
+const Add: React.FC<HomeProps> = ({ birthdayStore }) => {
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Add Birthday</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">Add Birthday</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonList>
+                    <IonItemDivider>Name</IonItemDivider>
+                    <IonItem>
+                        <IonInput />
+                    </IonItem>
+                    <IonItemDivider>Birthday</IonItemDivider>
+                    <IonItem>
+                        <IonInput />
+                    </IonItem>
+                </IonList>
+                <IonButton>Add</IonButton>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Add;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,14 @@ import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
-const Home: React.FC = () => {
+import { BirthdayStore } from "./BirthdayStore";
+
+type HomeProps = {
+  birthdayStore: BirthdayStore,
+};
+
+const Home: React.FC<HomeProps> = ({ birthdayStore }) => { 
+
   return (
     <IonPage>
       <IonHeader>
@@ -14,7 +21,7 @@ const Home: React.FC = () => {
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Birthdays</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
